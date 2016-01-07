a92bceb887b540b68b22f81ae7f4fd01b0e83286
Gaël UTARD <gael.utard@midipy.eedf.fr>
2016-01-07 13:44:34 +0100

    Activate manage.py with virtualenv

--- manage.py
+++ manage.py
@@ -2,6 +2,11 @@
 import os
 import sys
 
+activate_this = os.path.join(os.path.dirname(__file__), 'env/bin/activate_this.py')
+with open(activate_this) as f:
+    code = compile(f.read(), activate_this, 'exec')
+    exec(code, dict(__file__=activate_this))
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jeito.settings")
 
