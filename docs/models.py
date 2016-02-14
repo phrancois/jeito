4f9656d97179c641cfec756c5fee5c22c32fbda5
Gael UTARD <gael.utard@midipy.eedf.fr>
2016-02-14 15:01:46 +0100

    Upload documents in a separate directory for each day

--- docs/models.py
+++ docs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Document(models.Model):
-    file = models.FileField(u"Fichier", max_length=200)
+    file = models.FileField(u"Fichier", max_length=200, upload_to='docs/%Y/%m/%d/')
     title = models.CharField(u"Titre", max_length=200)
 
     def __str__(self):
