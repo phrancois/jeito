f96bb25c0135a8032df735530f21e40d0bed1366
Gael UTARD <gael.utard@midipy.eedf.fr>
2016-02-14 20:50:38 +0100

    Production setup

--- jeito/settings.py
+++ jeito/settings.py
@@ -119,6 +119,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
 
 HAYSTACK_CONNECTIONS = {
     'default': {
