5d7036afb0c6533e69763f766ee7f1c70891ad93
Gael UTARD <gael.utard@midipy.eedf.fr>
2016-02-16 22:18:49 +0100

    Use elasticsearch instead of whoosh as search backend

--- jeito/settings.py
+++ jeito/settings.py
@@ -130,8 +130,9 @@ STATIC_URL = '/static/'
 
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-        'PATH': os.path.join(BASE_DIR, 'var/whoosh_index'),
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
     },
 }
 HAYSTACK_SIGNAL_PROCESSOR = 'docs.signals.DocumentSignalProcessor'
