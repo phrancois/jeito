b78697d48065fcd3b004e8b79333b37b999e9103
Gaël UTARD <gael.utard@midipy.eedf.fr>
2016-02-17 21:47:01 +0100

    Move the base template to the project directory

--- jeito/settings.py
+++ jeito/settings.py
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'jeito.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'jeito', 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
