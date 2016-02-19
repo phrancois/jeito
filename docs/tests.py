4974ffdcfd12e9a72ea57b9e9ad86e239a9bc459
Gael UTARD <gael.utard@midipy.eedf.fr>
2016-02-20 00:03:01 +0100

    Test docs create view

--- docs/tests.py
+++ docs/tests.py
@@ -28,3 +28,7 @@ class LoggedTests(LoggedTestMixin, HaystackTestCase):
     def test_index_view(self):
         response = self.client.get('/docs/')
         self.assertContains(response, '<li class="active">Documents</li>')
+
+    def test_create_view(self):
+        response = self.client.get('/docs/create/')
+        self.assertContains(response, '<h1>Ajouter un document</h1>')
